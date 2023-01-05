03d21ca462d06b67ad9b9227bad970208f48cc2a
dericward <dericward@hotmail.com>
2023-01-05 22:19:56 +0000

    reduced number of screen alerts for a less annoying user experience

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,17 +8,17 @@ let ties = 0;
 
 let playAgain = true;
 
-// keep playing until the player presses cancel
+// keep playing until the player presses cancel when prompted after each game is finished
 while (playAgain === true) {
 
 // ask player for a choice
 playerChoice = prompt('Please enter your choice of rock, paper, or scissors: ');
 
-// select an item from the array at random
+// select rock, paper, or scissors at random
 let randomIndex = Math.floor(Math.random() * rps_choices.length);
 let computerChoice = rps_choices[randomIndex];
 
-// announce the choices made by the player and the computer
+// display the choices made by the player and the computer
 alert('You chose: ' + playerChoice + '. The computer chose: ' + computerChoice + '. '
     + 'The randomIndex value is: ' + randomIndex + '.');
 
@@ -29,30 +29,24 @@ if (((playerChoice === 'rock') && (computerChoice === 'scissors')) ||
     playerWins++;
     alert("You win!");
    }
-
 else if (((computerChoice === 'rock') && (playerChoice === 'scissors')) ||
         ((computerChoice === 'paper') && (playerChoice === 'rock')) ||
         ((computerChoice === 'scissors') && (playerChoice === 'paper'))) {
         computerWins++;
         alert("You lose!");
         }
-
 else {
     ties++;
     alert('it is a tie!');
    }
 
-// announce results
-alert("You have won " + playerWins + " times, and the computer has won " + computerWins + " times!");
-
-// play again or quit? 
-if (confirm('Press OK to play again or Cancel to quit!')) {
+// display results + play again or quit? 
+if (confirm('You have won ' + playerWins + ' times, and the computer has won ' + computerWins + ' times! Press OK to play again or Cancel to quit!')) {
     playAgain = true;
 } 
 else {
     playAgain = false;
 }
-
 }
 
 
