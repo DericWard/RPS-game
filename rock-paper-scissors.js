695198fc77930e48979bcbf4043bae039306b5fe
dericward <dericward@hotmail.com>
2023-01-05 22:03:50 +0000

    added comments to rock-paper-scissors.js

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -8,18 +8,21 @@ let ties = 0;
 
 let playAgain = true;
 
-
+// keep playing until the player presses cancel
 while (playAgain === true) {
 
+// ask player for a choice
 playerChoice = prompt('Please enter your choice of rock, paper, or scissors: ');
 
+// select an item from the array at random
 let randomIndex = Math.floor(Math.random() * rps_choices.length);
 let computerChoice = rps_choices[randomIndex];
 
+// announce the choices made by the player and the computer
 alert('You chose: ' + playerChoice + '. The computer chose: ' + computerChoice + '. '
     + 'The randomIndex value is: ' + randomIndex + '.');
 
-
+// check who won 
 if (((playerChoice === 'rock') && (computerChoice === 'scissors')) || 
     ((playerChoice === 'paper') && (computerChoice === 'rock')) || 
     ((playerChoice === 'scissors') && (computerChoice === 'paper'))) {
@@ -39,15 +42,16 @@ else {
     alert('it is a tie!');
    }
 
+// announce results
 alert("You have won " + playerWins + " times, and the computer has won " + computerWins + " times!");
 
+// play again or quit? 
 if (confirm('Press OK to play again or Cancel to quit!')) {
     playAgain = true;
 } 
 else {
     playAgain = false;
 }
-
 
 }
 
