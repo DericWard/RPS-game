740ed22c6e2476ad1cc4df085df330221910f415
dericward <dericward@hotmail.com>
2023-01-05 21:50:57 +0000

    reduced winner-check code and included a loop that only quits when the player selects the 'cancel' button at the end of each game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,67 +1,54 @@
 let rps_choices = ['rock', 'paper', 'scissors'];
 
-let randomIndex = Math.floor(Math.random() * rps_choices.length);
-
-let computerChoice = rps_choices[randomIndex];
 let playerChoice;
 
 let computerWins = 0;
-let playerWins =0;
+let playerWins = 0;
 let ties = 0;
 
-let playAgain = 0;
+let playAgain = true;
+
+
+while (playAgain === true) {
 
 playerChoice = prompt('Please enter your choice of rock, paper, or scissors: ');
 
-alert('You chose: ' + playerChoice);
-alert('The computer chooses: ' + computerChoice);
+let randomIndex = Math.floor(Math.random() * rps_choices.length);
+let computerChoice = rps_choices[randomIndex];
+
+alert('You chose: ' + playerChoice + '. The computer chose: ' + computerChoice + '. '
+    + 'The randomIndex value is: ' + randomIndex + '.');
 
 
-if (playerChoice === 'rock'){
-    if (computerChoice === 'rock') {
-        ties++;
-        alert('it is a tie!');
-    }
-    else if (computerChoice === 'paper') {
+if (((playerChoice === 'rock') && (computerChoice === 'scissors')) || 
+    ((playerChoice === 'paper') && (computerChoice === 'rock')) || 
+    ((playerChoice === 'scissors') && (computerChoice === 'paper'))) {
+    playerWins++;
+    alert("You win!");
+   }
+
+else if (((computerChoice === 'rock') && (playerChoice === 'scissors')) ||
+        ((computerChoice === 'paper') && (playerChoice === 'rock')) ||
+        ((computerChoice === 'scissors') && (playerChoice === 'paper'))) {
         computerWins++;
-        alert('You lose!');
-    }
-    else if (computerChoice === 'scissors') {
-        playerWins++;
-        alert('You win!');
-    }
-}
+        alert("You lose!");
+        }
 
-if (playerChoice === 'paper'){
-    if (computerChoice === 'paper') {
-        ties++;
-        alert('it is a tie!');
-    }
-    else if (computerChoice === 'scissors') {
-        computerWins++;
-        alert('You lose!');
-    }
-    else if (computerChoice === 'rock') {
-        playerWins++;
-        alert('You win!');
-    }
-}
-
-if (playerChoice === 'scissors'){
-    if (computerChoice === 'scissors') {
-        ties++;
-        alert('it is a tie!');
-    }
-    else if (computerChoice === 'rock') {
-        computerWins++;
-        alert('You lose!');
-    }
-    else if (computerChoice === 'paper') {
-        playerWins++;
-        alert('You win!');
-    }
-}
+else {
+    ties++;
+    alert('it is a tie!');
+   }
 
 alert("You have won " + playerWins + " times, and the computer has won " + computerWins + " times!");
+
+if (confirm('Press OK to play again or Cancel to quit!')) {
+    playAgain = true;
+} 
+else {
+    playAgain = false;
+}
+
+
+}
 
 
